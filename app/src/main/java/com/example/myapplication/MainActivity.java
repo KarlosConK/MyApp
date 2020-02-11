cf3e59a839871e8c2410c9c010ff764c1509d5f7
Carlos Sánchez Riaza <sanchez.riaza.carlos_@hotmail.com>
2020-02-11 16:54:43 +0100

    un comentario suelto

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
+//toma comentario
         return super.onOptionsItemSelected(item);
     }
 }
